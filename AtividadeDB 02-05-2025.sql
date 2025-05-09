4744abbaa90b97bcd7ff7366616db8d097510354
Guilherme Duarte Siqueira <guilherme.duarte.exe@gmail.com>
2025-05-09 20:23:34 -0300

    AtividadeDB 02-05-2025.sql
    Corrigir erros de sintaxe na criação de tabelas com chave estrangeira.

--- AtividadeDB 02-05-2025.sql
+++ AtividadeDB 02-05-2025.sql
@@ -1,7 +1,7 @@
 -- Criando banco de dados
 
 CREATE DATABASE School_Server;
-USE School_Server;
+USE school_Server;
 
 -- Inserindo tabelas
 
@@ -31,27 +31,45 @@ CREATE TABLE Matriculas_cl (
 );
 
 -- Modificações e consultas
+SHOW TABLES;
+SHOW DATABASES;
 
 ALTER TABLE Alunos_cl ADD email VARCHAR(80);
 ALTER TABLE Cursos_Senai MODIFY COLUMN carga_horaria SMALLINT;
 
+UPDATE Alunos_cl
+SET telefone = '1122339958'
+WHERE id_aluno = 101;
+
+DELETE FROM Cursos_Senai WHERE nome_curso = 'Operador de Empilhadeira';
 DROP TABLE Matricula_cl;
 
 -- Adicionando dados nas tabelas
 
 INSERT INTO Alunos_cl (id_aluno, nome, CPF, data_nascimento, telefone, email)
 VALUES
-(101, 'Carlos Alberto', 22345653812, '19-04-1999', '11978515657', 'carloscbl@email.com'),
-(102, 'Marta Silva', 11190653890, '11-05-1985', '12985564433', 'msilva@email.com'),
-(103, 'José Roberto', 30345653900, '22-03-2003', '11995724650', 'jjrobertinho@email.com'),
-(104, 'Maria Amélia', 45045253902, '03-01-2000', '73955515690', 'marialia@email.com');
+(101, 'Carlos Alberto', 22345653812, '1999-03-12', '11978515657', 'carloscbl@email.com'),
+(102, 'Marta Silva', 11190653890, '1985-04-20', '12985564433', 'msilva@email.com'),
+(103, 'José Roberto', 30345653900, '2003-06-19', '11995724650', 'jjrobertinho@email.com'),
+(104, 'Maria Amélia', 45045253902, '2000-01-23', '73955515690', 'marialia@email.com');
 
 INSERT INTO Cursos_Senai (id_curso, nome_curso, carga_horaria, data_inicio, data_termino)
 VALUES
-(201, 'Auxiliar de Finanças', 50, '11-03-2024', '28-04-2024'),
-(202, 'Auxiliar de Recursos Humanos', 120, '08-02-2024', '25-05-2024'),
-(203, 'Sistemas Elétricos', 80, '08-06-2024', '17-08-2024'),
-(204, 'Operador de Empilhadeira', 100, '03-05-2024', '04-06-2024');
+(201, 'Auxiliar de Finanças', 50, '2024-05-08', '2024-07-26'),
+(202, 'Auxiliar de Recursos Humanos', 120, '2024-02-08', '2024-05-28'),
+(203, 'Sistemas Elétricos', 80, '2024-03-08', '2024-04-20'),
+(204, 'Operador de Empilhadeira', 100, '2024-11-08', '2025-01-26');
 
+INSERT INTO Matricula_cl (id_aluno, id_curso, data_matricula)
+VALUES
+(101, 201, '2024-05-01'),
+(102, 202, '2024-02-01'),
+(103, 203, '2024-03-01'),
+(104, 204, '2024-11-01');
 
+-- Seleção de dados
 
+SELECT * FROM Alunos_cl;
+SELECT * FROM Cursos_Senai WHERE carga_horaria > 70;
+SELECT * FROM Matricula_cl WHERE data_matricula > '01-03-2024'
+SELECT nome, data_nascimento FROM Alunos_cl;
